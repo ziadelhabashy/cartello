d5aedbf35c6b5af44e2a7f596457758b9c4e18b4
Youssef Ahmed <youssef.youssef88888@gmail.com>
2026-06-07 14:46:14 +0300

    Update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -6,7 +6,7 @@ const upload            = require('../upload');
 router.get('/products',              productController.getAllProducts);
 router.get('/admin/products',        productController.adminGetAllProducts);
 router.post('/admin/products',       upload.single('image'), productController.adminAddProduct);
-router.put('/admin/products/:id', uplaod.single('image'), productController.adminUpdateProduct);
+router.put('/admin/products/:id', upload.single('image'), productController.adminUpdateProduct);
 router.delete('/admin/products/:id', productController.adminDeleteProduct);
 
 module.exports = router;
