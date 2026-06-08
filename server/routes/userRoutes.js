1569a7b4a2edbc73898835f3a0c129379cb1743a
Rahaff mohsenn <rahaf2404655@miuegypt.edu.eg>
2026-06-08 20:00:37 +0300

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -18,7 +18,7 @@ router.post('/remove-address',    userController.removeAddress);
 // Admin
 router.post('/admin/login', userController.adminLogin);
 router.get('/admin/users',isAdmin, userController.adminGetAllUsers);
-router.delete('/admin/users', isAdmin, userController.adminDeleteAllUsers);
+router.delete('/admin/users/:id',isAdmin, userController.adminDeleteUser);
 router.post('/forgot-password', userController.forgotPassword);
 router.post('/reset-password', userController.resetPassword);
 
