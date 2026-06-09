762d7c803e407889de0e08d4eb89acca3e0a686c
Youssef Ahmed <youssef.youssef88888@gmail.com>
2026-06-09 06:59:50 +0300

    feat: add fileFilter and size limit to multer upload config

--- server/upload.js
+++ server/upload.js
@@ -1,9 +1,18 @@
 const multer = require('multer');
 const path   = require('path');
 
-const storage = multer.diskStorage ({
+const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
   filename:    (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
 });
 
-module.exports = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) cb(null, true);
+  else cb(new Error('Only images allowed'));
+};
+
+module.exports = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }
+});
